59fa2b7e998e9a53e059a0223d4d668c46566619
Darkshipreturn <74745096+Darkshipreturn@users.noreply.github.com>
2021-03-26 14:48:39 +1100

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

---
+++ tilemap.g.ts
@@ -0,0 +1,81 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`10001000020303030305020303030305030303040a0f0e0e0e0e0a0e0e0e0e0e0e0e0f0c010e0e0e0e0e0a0e0e0e0e0f0e0e0e060a0e0e020303100f0e120909110e0e060a0e0e0a0e0e0e0e0e060f0e0a0e0e060a0f0e0a0e0e0f0e0e060e0e0a0e0e060d0e0e0a0e0e0a0f0e060e0f0a0f0e0c0a0e0e08110f0a0e0e060f0e0a0e0e060a0e0f0e08090a0e0e060e0e0a0e0e060a0e0e0e0f0e0a0f0e060e0e0a0f0e060a0e0e0e0e0e0a0e0e060e0e0a0e0e060a0e0e030303100e0f0e0e0f0a0e0e060d0e0e0e0e0e0f0e0e0e0e0e0a0e0e0c0a0e0e0e0e0e0a0e0e0e0e0e0a0e13060a0f0e0e0e0f0a0e0e0f0e0e0a0f0e060809090b09090809090b090908090907`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . 2 . . . . . . . . 2 
+. . . . . . 2 . . . . . . . . 2 
+2 . . 2 2 2 2 . . 2 2 2 2 . . 2 
+2 . . 2 . . . . . 2 . . 2 . . 2 
+2 . . 2 . . . . . 2 . . 2 . . 2 
+2 . . 2 . . 2 . . 2 . . 2 . . 2 
+2 . . 2 2 . 2 . . 2 . . 2 . . 2 
+2 . . . 2 2 2 . . 2 . . 2 . . 2 
+2 . . . . . 2 . . 2 . . 2 . . 2 
+2 . . . . . 2 . . 2 . . 2 . . 2 
+2 . . 2 2 2 2 . . . . . 2 . . 2 
+2 . . . . . . . . . . . 2 . . 2 
+2 . . . . . 2 . . . . . 2 . . 2 
+2 . . . . . 2 . . . . . 2 . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.stairEast,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterSouth2,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterWest2,sprites.dungeon.floorDark2,sprites.dungeon.floorDark5,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.stairLadder], TileScale.Sixteen);
+            case "level3":
+            case "level3":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
